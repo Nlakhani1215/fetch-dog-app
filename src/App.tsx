e6855e47389d9492abe2c79861cd39d964b912db
Nlakhani1215 <nashitalakhani1234@>
2025-05-16 21:43:12 -0500

    Fix build issues for Vercel deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-
 import LoginPage from '@pages/LoginPage';
 import SearchPage from '@pages/SearchPage';
 import FavoritesPage from '@pages/FavoritesPage';
